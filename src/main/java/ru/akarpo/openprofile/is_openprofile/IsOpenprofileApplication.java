c55b6fd2895c5a5d244876a91fcc9ea92e95de99
AkkaRin11 <artemcrepper@gmail.com>
2025-11-16 13:53:00 +0300

    init

---
+++ src/main/java/ru/akarpo/openprofile/is_openprofile/IsOpenprofileApplication.java
@@ -0,0 +1,13 @@
+package ru.akarpo.openprofile.is_openprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsOpenprofileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsOpenprofileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/akarpo/openprofile/is_openprofile/IsOpenprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akarpo.openprofile.is_openprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsOpenprofileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
