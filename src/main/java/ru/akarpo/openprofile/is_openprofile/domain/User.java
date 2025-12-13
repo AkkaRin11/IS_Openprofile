35ff5ab4f32588fc62baa56c5c4dea369276b030
AkkaRin11 <artemcrepper@gmail.com>
2025-12-13 16:52:00 +0300

    add default user ROLE and fix passwordEncoder in SecurityConfig

--- src/main/java/ru/akarpo/openprofile/is_openprofile/domain/User.java
+++ src/main/java/ru/akarpo/openprofile/is_openprofile/domain/User.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
@@ -32,6 +33,7 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
+    @ColumnDefault("'USER'")
     @Builder.Default
     private Role role = Role.USER;
 
